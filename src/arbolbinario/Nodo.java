9a131d5e914eb01fa8c0e0a48dd48489c830ecb9
Estuardo2012381 <josu.lopez.barrios@gmail.com>
2020-03-25 10:08:55 -0600

    Creacion del proyecto

---
+++ src/arbolbinario/Nodo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolbinario;
+
+/**
+ *
+ * @author Estuardo
+ */
+public class Nodo {
+    private int dato;
+    private Nodo izq,der;
+    
+    public Nodo(int dato){
+        
+    }
+    
+    
+    
+}
