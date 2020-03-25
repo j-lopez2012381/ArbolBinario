016aa07294576f25ffe7d8cbac6e92cd0277ca93
Estuardo2012381 <josu.lopez.barrios@gmail.com>
2020-03-25 11:47:04 -0600

    Avances con la Segunda clase

--- src/arbolbinariografico/ArbolBin.java
+++ src/arbolbinariografico/ArbolBin.java
@@ -44,6 +44,74 @@ public class ArbolBin {
             else
                 anterior.setDer(nuevo);
         }
+        
+        numNodos++;
     }
+
+    public Nodo getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodo raiz) {
+        this.raiz = raiz;
+    }
+
+    public int getNumNodos() {
+        return numNodos;
+    }
+    
+    public void preorden(Nodo aux, LinkedList recorrido){
+        if(aux != null){
+            recorrido.add(aux.getDato());
+            preorden (aux.getIzq(), recorrido);
+            preorden (aux.getDer(), recorrido);
+        }
+    }
+    
+    public void inorden(Nodo aux, LinkedList recorrido){
+        if(aux != null){
+            inorden (aux.getIzq(), recorrido);
+            recorrido.add(aux.getDato());
+            inorden (aux.getDer(), recorrido);
+        }
+    }
+    
+    public void postorden(Nodo aux, LinkedList recorrido){
+        if(aux != null){
+            postorden (aux.getIzq(), recorrido);
+            postorden (aux.getDer(), recorrido);
+            recorrido.add(aux.getDato());
+        }
+    }
+    
+    public void porNivel(Nodo aux, LinkedList recorrido){
+        LinkedList<Nodo> cola = new LinkedList<Nodo>();
+        cola.addLast(aux);
+        while(cola.size() > 0){
+            Nodo tmp = cola.pollFirst();
+            recorrido.add(tmp.getDato());
+            if(tmp.getIzq() != null){
+                cola.addLast(tmp.getIzq());
+            }
+            if(tmp.getDer() != null){
+                cola.addLast(tmp.getDer());
+            }
+        }
+    }
+    
+    public boolean existe(int dato){
+        Nodo aux = raiz;
+        while(aux != null){
+            if(dato == aux.getDato())
+                return true;
+            else
+                if(dato>aux.getDato())
+                    aux=aux.getDer();
+                else 
+                    aux=aux.getIzq();
+        }
+        return false;
+    }
+    
     
 }
